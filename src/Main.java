572ee3e51b9d7df07cdda666baa9596d72fe2ea5
MarceloBcosta <mbeckercosta@gmail.com>
2026-06-11 16:27:11 -0300

    atividade alura concluida

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+void main() {
+
+
+    /*boolean intermaiorqgremio = true;                                         // esse é um fato
+       boolean gremiomenorqinter = true;
+            int dia = 11;
+
+            String info; info= " hoje comerei pastel";
+
+    System.out.println( "sim esse é o dia:" + dia ) ; if ( intermaiorqgremio || gremiomenorqinter)  ;
+    System.out.println( "esse será um grande dia," + info + "!" ) ; if ( gremiomenorqinter = true ) ;/*
+
+     */
+    System.out.println("Aqui vai a temperatura dos proximos dias!" );
+
+    int temperaturad1 = 18;
+    int temperaturad2= 34;
+    int temperaturad3= 56;
+    int temperaturad4 = 10;
+    String valortermico= " fahrenheit";
+
+    System.out.println((( "dia primeiro: " ) + temperaturad1 * 1.8) + 32 + valortermico);
+    System.out.println((( "segundo dia: "  ) + temperaturad2 * 1.8) + 32 + valortermico);
+    System.out.println((( "terceiro dia: " ) + temperaturad3 * 1.8) + 32 + valortermico);
+    System.out.println((( "quarto dia: " ) + temperaturad4 * 1.8) + 32 + valortermico);
+
+   double fah1 = 64.4;
+    double fah2 = 93.2;
+    double fah3 = 132.8;
+   double fah4 = 50.0;
+
+    int mediafahrenheit = (int) (fah1 + fah2 + fah3 + fah4 ) /4;
+
+    String mensagem = "A media de fahrenheit dos proximos dias será: ";
+
+    System.out.println(mensagem + mediafahrenheit);
+}
---
+++ src/atividade1.java
@@ -0,0 +1,23 @@
+public class atividade1 {
+
+}
+
+
+/*Agora é com você! Pratique os conceitos que foram ensinados ao longo dessa aula com o seguinte desafio:
+Escreva um programa que converta uma temperatura em graus Celsius para Fahrenheit. Utilize variáveis para representar os valores das temperaturas e imprima no console o valor convertido de Celsius para Fahrenheit.
+Dica: A fórmula para converter temperaturas de graus Celsius para Fahrenheit é: (temperatura * 1.8) + 32.
+Depois de finalizar, testar e conferir que seu programa foi executado com sucesso,
+crie uma variável inteira para exibir a temperatura em Fahrenheit sem casas decimais. Lembre-se que provavelmente você precisará fazer um casting de valores./*
+ */
+
+void main() {
+
+}
+
+   int temperaturad1 = 18;
+     int temperaturad2= 34;
+      int temperaturad3= 56;
+       int temperaturad4 = 10;
+     String valortermico= "graus";
+
+
